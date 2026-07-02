8024547a1cb1fd75f4faffc8cf86fc5b8dc92f1f
Joshua Sanicharan <jsanicha@uwaterloo.ca>
2026-07-02 11:11:41 -0400

    added career hitting stats endpoint

--- backend/app/services/mlb_client.py
+++ backend/app/services/mlb_client.py
@@ -4,3 +4,8 @@ async def search_player(name:str):
     async with httpx.AsyncClient() as client:
         response = await client.get("https://statsapi.mlb.com/api/v1/people/search", params ={"names" : name})
         return response.json()
+    
+async def get_career_offense_stats(player_id:int):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"https://statsapi.mlb.com/api/v1/people/{player_id}/stats", params={"stats": "yearByYear", "group": "hitting"})
+        return response.json()
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI 
 from app.services.mlb_client import search_player
-
+from app.services.mlb_client import get_career_offense_stats
 app = FastAPI()
 @app.get("/")
 def read_root():
@@ -9,3 +9,9 @@ def read_root():
 async def search_players(name:str):
     result = await search_player(name)
     return result
+
+@app.get("/players/{player_id}/stats")
+async def hitting_stats(player_id : int):
+    result = await get_career_offense_stats(player_id)
+    return result
+
