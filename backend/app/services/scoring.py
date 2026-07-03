77cfac9e8cb6a9121cf59697ee50328edd29d05e
Joshua Sanicharan <jsanicha@uwaterloo.ca>
2026-07-03 10:36:36 -0400

    added iso function

--- backend/app/services/scoring.py
+++ backend/app/services/scoring.py
@@ -27,3 +27,22 @@ def get_plate_discipline(seasons: list) -> float:
         return 0 
     else:
         return walks/strikeouts
+
+def get_calculated_iso(seasons:list ) -> float:
+    decay = 0.7
+    weighted_sum = 0
+    total_weight = 0
+
+
+    seasons_reversed_list = list(reversed(seasons))
+    for i in range(len(seasons_reversed_list)):
+        season = seasons_reversed_list[i]
+        season_avg = float(season["stat"]["avg"])
+        season_slg = float(season["stat"]["slg"])
+        weight = decay ** i
+        iso = season_slg - season_avg
+        weighted_sum = weighted_sum + (iso * weight)
+        total_weight = total_weight + weight 
+        
+
+    return weighted_sum / total_weight
