e988737696c042940b5001fce5c19f393fdec09f
Joshua Sanicharan <jsanicha@uwaterloo.ca>
2026-07-06 09:53:04 -0400

    added standard deviation

--- backend/app/services/scoring.py
+++ backend/app/services/scoring.py
@@ -1,3 +1,4 @@
+import math
 def get_calculated_ops(seasons:list ) -> float:
     decay = 0.7
     weighted_sum = 0
@@ -46,3 +47,24 @@ def get_calculated_iso(seasons:list ) -> float:
         
 
     return weighted_sum / total_weight
+
+def get_mean(values: list) -> float:
+    total = 0 
+    for i in range(len(values)):
+        total += values[i]
+    return total / len(values)
+
+def get_variance(values:list) -> float:
+    total = 0
+    average = get_mean(values)
+    for i in range(len(values)):
+        deviation = (average - values[i])**2
+        total += deviation
+    return total / len(values)
+def get_standard_deviation(values:list) -> float:
+    result = get_variance(values)
+    return math.sqrt(result)
+
+def get_consistency_score(values: list) -> float:
+    result = get_standard_deviation(values)
+    return 1 / (1+result)
